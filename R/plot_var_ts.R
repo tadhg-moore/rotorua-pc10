77a8ff790b180d9ea5ff66af37ea6196bf3b0a82
Tadhg Moore <tadhg.moore6@gmail.com>
2025-12-01 17:46:45 +1300

    Add missing arg

--- R/plot_var_ts.R
+++ R/plot_var_ts.R
@@ -1,5 +1,5 @@
 # Function to plot GCM spatial data
-plot_var_ts <- function(gcm_ts_df, variable = "tas", files) {
+plot_var_ts <- function(gcm_ts_df, variable = "tas", metadata, files) {
   # List all matching files
   # files <- list.files(
   #   dir, 
@@ -7,6 +7,9 @@ plot_var_ts <- function(gcm_ts_df, variable = "tas", files) {
   #   full.names = TRUE
   # )
   
+  var_meta <- metadata |> 
+    dplyr::filter(variable == !!variable) |>
+    dplyr::distinct(variable, .keep_all = TRUE)
   
   gcms <- c("ACCESS-CM2", "AWI-CM-1-1-MR", "CNRM-CM6-1", "EC-Earth3", 
             "GFDL-ESM4", "NZESM", "NorESM2-MM")
@@ -14,11 +17,11 @@ plot_var_ts <- function(gcm_ts_df, variable = "tas", files) {
   
   hist_df <- gcm_ts_df |> 
     dplyr::filter(variable == !!variable &
-                    scenario %in% c("historical", "Historical"))
+                    scenario %in% c("historical"))
   scen_all <- gcm_ts_df |> 
     dplyr::filter(variable == !!variable &
-                    !(scenario %in% c("historical", "Historical")))
-
+                    !(scenario %in% c("historical")))
+  
   ref <- hist_df |> 
     dplyr::group_by(gcm) |>
     dplyr::summarise(
@@ -28,24 +31,28 @@ plot_var_ts <- function(gcm_ts_df, variable = "tas", files) {
   
   
   all <- dplyr::bind_rows(hist_df, scen_all) |> 
+    dplyr::left_join(ref, by = "gcm") |>
     dplyr::mutate(
       anom = value - ref_val,
-      scenario = factor(scenario, levels = c("Historical", sort(unique(scen_all$scenario))))
+      scenario = factor(scenario, levels = c("historical", sort(unique(scen_all$scenario))))
     )
   
   p <- ggplot2::ggplot() +
     ggplot2::geom_vline(xintercept = 2015, linetype = "dotted") +
     ggplot2::geom_hline(yintercept = 0, linetype = "dashed") +
-    ggplot2::geom_line(data = all, ggplot2::aes(x = year, y = delta, 
-                                                color = gcm)) +
-    ggplot2::facet_grid(~scenario) +
-    ggplot2::geom_smooth()
-  ggplot2::labs(
-    title = paste0("Annual Mean ", variable, " Anomalies for ", gcm),
-    x = "Year",
-    y = expression(Delta~from~Historical~Mean),
-    color = "Scenario"
-  ) +
+    ggplot2::geom_line(data = all, ggplot2::aes(x = year, y = anom, 
+                                                color = gcm), alpha = 0.4) +
+    ggplot2::facet_grid(~scenario, scales = "free_x") +
+    ggplot2::geom_smooth(data = all, 
+                         ggplot2::aes(x = year, y = anom, group = scenario),
+                         method = "loess", color = "black", se = TRUE) +
+    ggplot2::labs(
+      title = paste0("Annual Mean ", var_meta$variable_name_long,
+                     " Anomalies"),
+      x = "Year",
+      y = expression(Delta~from~Historical~Mean),
+      color = "Scenario"
+    ) +
     ggplot2::theme_minimal()
   
   p
