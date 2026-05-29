de3cfc796ae376adf38d8cc1d8bba9bf72316449
tadhg-moore <tadhg.moore6@gmail.com>
2026-05-29 14:43:01 +1200

    Update pre-render

--- website/scripts/download_aeme_api.R
+++ website/scripts/download_aeme_api.R
@@ -1,4 +1,7 @@
-aeme <- aemetools::get_aeme(id = "LID11133")
-
-aeme <- AEME::build_aeme(aeme = aeme, path = "../") |> 
-  AEME::run_aeme()
+chk <- aemetools::check_api_status()
+if (chk) {
+  aeme <- aemetools::get_aeme(id = "LID11133")
+  
+  aeme <- AEME::build_aeme(aeme = aeme, path = "../") |> 
+    AEME::run_aeme()
+}
