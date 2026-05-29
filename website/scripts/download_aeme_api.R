b69500c5b65aae1d8ee2edb81dca75f7a6875831
tadhg-moore <tadhg.moore6@gmail.com>
2026-05-29 15:00:38 +1200

    Add saving the file

--- website/scripts/download_aeme_api.R
+++ website/scripts/download_aeme_api.R
@@ -1,6 +1,7 @@
 chk <- aemetools::check_api_status()
 if (chk) {
   aeme <- aemetools::get_aeme(id = "LID11133")
+  saveRDS(aeme, "../LID11133_rotorua/aeme_download.rds")
   
   aeme <- AEME::build_aeme(aeme = aeme, path = "../") |> 
     AEME::run_aeme()
